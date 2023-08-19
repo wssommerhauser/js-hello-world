d6f90849495e1850a31a15aee6f3115519b7cfca
William Sommerhauser <wssommerhauser@gmail.com>
2023-08-19 15:36:33 -0500

    Output text modified.

--- hello.js
+++ hello.js
@@ -4,7 +4,8 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Quoth the Raven");
+console.log("Nevermore, World.");
 
 /*  
 Next, PLAY! 
